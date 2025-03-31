1f21b291eda7e59254b048d3c5ed44416c083777
Pablo <pablogomesph55@gmail.com>
2025-03-31 18:26:21 -0300

    Add files via upload

---
+++ Intro-React/src/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./style.css"
+
+const Botao = () => {
+  return <button className="">Enviar</button>;
+};
+
+const Navbar = () => {
+  return (
+    <nav>
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <Botao />
+    </nav>
+  );
+};
+
+export default Navbar;
